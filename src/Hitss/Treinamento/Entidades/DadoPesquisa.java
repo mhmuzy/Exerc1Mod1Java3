13adfa934f13fb6c335c901e3cb548a2a283fbf7
marcio henrique <mhmuzy@embratel.com.br>
2021-04-07 12:51:11 -0300

    Criação dos Vetores [Treinamento]

---
+++ src/Hitss/Treinamento/Entidades/DadoPesquisa.java
@@ -0,0 +1,28 @@
+package Hitss.Treinamento.Entidades;
+
+public class DadoPesquisa {
+	private int idade;
+	private String sexo;
+	private double salario;
+	
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getSexo() {
+		return sexo;
+	}
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	
+}
---
+++ src/Hitss/Treinamento/Repositorios/DadoPesquisaRepositorio.java
@@ -0,0 +1,18 @@
+package Hitss.Treinamento.Repositorios;
+
+public class DadoPesquisaRepositorio {
+	
+	public void CalcularMediaSalarial() {
+		
+		int[] Idade = new int[5];
+		Idade[1] = 26;
+		Idade[2] = 36;
+		Idade[3] = 30;
+		Idade[4] = 28;
+		
+		String[] Sexo = new String[5];
+		Sexo[1] = "M";
+		Sexo[2] = "M";
+		
+	}
+}
