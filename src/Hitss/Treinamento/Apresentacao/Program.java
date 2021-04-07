009d36cf22fbfe9d4e9abd498dd1ea9fb698e85f
marcio henrique <mhmuzy@embratel.com.br>
2021-04-07 20:44:01 -0300

    Criação do Método de Média Salarial [Treinamento]

--- src/Hitss/Treinamento/Apresentacao/Program.java
+++ src/Hitss/Treinamento/Apresentacao/Program.java
@@ -1,10 +1,16 @@
 package Hitss.Treinamento.Apresentacao;
 
+import Hitss.Treinamento.Repositorios.*;
+
 public class Program {
 	public static void main(String[] args) {
 		System.out.println("");
 		System.out.println("Hitss Treinamento:");
 		System.out.println("");
 		System.out.println("Prezado(a) Usuário(a), estamos em Desenvolvimento no momento.");
+		
+		DadoPesquisaRepositorio rep = new DadoPesquisaRepositorio();
+		rep.CalcularMediaSalarial();
+		
 	}
 }
--- src/Hitss/Treinamento/Repositorios/DadoPesquisaRepositorio.java
+++ src/Hitss/Treinamento/Repositorios/DadoPesquisaRepositorio.java
@@ -13,6 +13,18 @@ public class DadoPesquisaRepositorio {
 		String[] Sexo = new String[5];
 		Sexo[1] = "M";
 		Sexo[2] = "M";
+		Sexo[3] = "F";
+		Sexo[4] = "F";
 		
+		double[] Salario = new double[5];
+		Salario[1] = 2000.00;
+		Salario[2] = 4000.00;
+		Salario[3] = 1200.00;
+		Salario[4] = 1600.00;
+		double mediaSalarial = (Salario[1] + Salario[2] + Salario[3] + Salario[4])/4; 
+		 
+		System.out.println("");
+		System.out.println("A média salarial é de R$ " + mediaSalarial);
+
 	}
 }
