50a6eb1d619412619833b4347098f0e7cf3b85f1
marcio henrique <mhmuzy@embratel.com.br>
2021-04-28 20:16:53 -0300

    Resolveu o Bug

--- src/Hitss/Treinamento/Repositorios/DadoPesquisaRepositorio.java
+++ src/Hitss/Treinamento/Repositorios/DadoPesquisaRepositorio.java
@@ -1,5 +1,9 @@
 package Hitss.Treinamento.Repositorios;
 
+import java.util.Arrays;
+
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane.MaximizeAction;
+
 public class DadoPesquisaRepositorio {
 	
 	public void CalcularMediaSalarial() {
@@ -21,10 +25,29 @@ public class DadoPesquisaRepositorio {
 		Salario[2] = 4000.00;
 		Salario[3] = 1200.00;
 		Salario[4] = 1600.00;
-		double mediaSalarial = (Salario[1] + Salario[2] + Salario[3] + Salario[4])/4; 
-		 
+		
+
+		
+		
+		
+		
+		Arrays.sort(Idade);
+		int maior = Idade[4]; 
+		int menor = Idade[1];
+		System.out.println("Menor Idade é : " + menor); 	
+		  System.out.println("Maior Idade é : " + maior);
+		 int media = 0;
+	        for(int i = 0; i<Salario.length; i++)
+	            media += Salario[i];
+	        
+	        float total = (float)media / (Salario.length - 1);
+	        System.out.println("A média é: "+total);
+	    }
+		
+		//double mediaSalarial = (Salario[1] + Salario[2] + Salario[3] + Salario[4])/4; 
+		 /*
 		System.out.println("");
 		System.out.println("A média salarial é de R$ " + mediaSalarial);
 
-	}
+	}*/
 }
